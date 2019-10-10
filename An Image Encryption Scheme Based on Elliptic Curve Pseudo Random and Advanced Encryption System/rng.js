4d616cbf7ef33d7176f1f068532f82e4f0703104
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-10-10 11:26:50 +0100

    write some ECC functions and RNG

---
+++ An Image Encryption Scheme Based on Elliptic Curve Pseudo Random and Advanced Encryption System/rng.js
@@ -0,0 +1,16 @@
+const { ecc_add, ecc_mul, ecc_y} = require('../utils/ecc')
+
+function rng(curve, primary_key, alpha, Y, G) {
+  let [a, b, n] = curve
+  let SK = [], Z = []
+  let A, B, C
+  SK.push(primary_key)
+  for (let i = 0; i < alpha; i++) {
+    A = ecc_mul(a, b, n, G, SK[i])
+    B = ecc_add(a, b, n, A, Y)
+    C = ecc_add(a, b, n, B, G)
+    Z.push(Math.abs(A[0] * B[0] * C[0]))
+    SK.push(A[1] + B[1] + C[1])
+  }
+  return Z
+}
---
+++ utils/ecc.js
@@ -0,0 +1,43 @@
+/*
+  All these operations are performed on the curve defined by:
+  y^2 = x^3 + ax + b
+  on [-n, n] interval
+*/
+
+// find the y coordinate of x on the curve
+function ecc_y(a, b, n, x) {
+  return Math.sqrt(x**3 + a * x + b) % n
+}
+
+// add two points p1 & p2 on the curve
+function ecc_add(a, b, n, p1, p2) {
+  let [x1, y1] = p1
+  let [x2, y2] = p2
+  let x, y, beta
+  
+  if (x1 == x2 && y1 == y2) {
+    beta = (3 * (x1 ** 2) + a) / (2 * y1) % n
+  } else {
+    beta = ((y2 - y1) / (x2 - x1)) % n
+  }
+  x = (beta ** 2 - x1 - x2) % n
+  y = (beta * (x1 - x) - y1) % n
+  return [x, y]
+}
+
+// multiply a point p with a scalar k on the curve
+// y ** 2 = x ** 3 + ax + b in [-n, n] interval
+
+function ecc_mul(a, b, n, p, k) {
+  let [x, y] = p
+  for (let i = 2; i <= k; i++) {
+    [x, y] = ecc_add(a, b, n, [x, y], [x, y])
+  }
+  return [x, y]
+}
+
+module.exports = {
+  ecc_add,
+  ecc_mul,
+  ecc_y
+}
