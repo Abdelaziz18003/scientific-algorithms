6278af62a7e07cededad97c849229d91ccc2c5c9
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-10-17 16:48:25 +0100

    run encryption or decryption based on user input

--- A new color image encryption using combination of the 1D chaotic map/index.js
+++ A new color image encryption using combination of the 1D chaotic map/index.js
@@ -8,6 +8,7 @@ const corr2 = require('ndarray-corr2')
 const entropy = require('ndarray-entropy')
 
 const encrypt = require('./encrypt')
+const decrypt = require('./decrypt')
 
 // secret parameters
 const secretKey = {
@@ -18,23 +19,54 @@ const secretKey = {
   lp: 600
 }
 
-getPixels('../images/lena_gray_256.png', (err, pixels) => {
-  if (err) throw err
-  pixels = pixels.pick(null, null, 0)
-  let plainImage = pool.clone(pixels)
-  let cipherImage = encrypt(pixels, secretKey)
+if (process.argv.includes('decryption')) {
+  decryption()
+} else {
+  encryption()
+}
+
+function encryption () {
+  getPixels('../images/lena_gray_256.png', (err, pixels) => {
+    if (err) throw err
+    pixels = pixels.pick(null, null, 0)
+    let plainImage = pool.clone(pixels)
+    let cipherImage = encrypt(pixels, secretKey)
+    
+    // analytics
+    console.log('Correlation coeff:', corr2(cipherImage, plainImage))
+    console.log('Plain image entropy:', entropy(plainImage))
+    console.log('Cipher image entropy:', entropy(cipherImage))
   
-  // analytics
-  console.log('Correlation coeff:', corr2(cipherImage, plainImage))
-  console.log('Plain image entropy:', entropy(plainImage))
-  console.log('Cipher image entropy:', entropy(cipherImage))
+    imhist(cipherImage)
+    imhist(plainImage)
+  
+    imshow(cipherImage, {gray: true})
+    imshow(plainImage, {gray: true})
+  
+    // save the encrypted image
+    savePixels(cipherImage, 'png').pipe(fs.createWriteStream('cipher-image.png'))
+  })
+}
 
-  imhist(cipherImage)
-  imhist(plainImage)
-
-  imshow(cipherImage, {gray: true})
-  imshow(plainImage, {gray: true})
-
-  // save the encrypted image
-  savePixels(cipherImage, 'png').pipe(fs.createWriteStream('cipher-image.png'))
-})
+function decryption () {
+  getPixels('./cipher-image.png', (err, pixels) => {
+    if (err) throw err
+    pixels = pixels.pick(null, null, 0)
+    let cipherImage = pool.clone(pixels)
+    let plainImage = decrypt(pixels, secretKey)
+    
+    // analytics
+    console.log('Correlation coeff:', corr2(plainImage, cipherImage))
+    console.log('Cipher image entropy:', entropy(cipherImage))
+    console.log('Plain image entropy:', entropy(plainImage))
+  
+    imhist(plainImage)
+    imhist(cipherImage)
+  
+    imshow(plainImage, {gray: true})
+    imshow(cipherImage, {gray: true})
+  
+    // save the encrypted image
+    savePixels(plainImage, 'png').pipe(fs.createWriteStream('plain-image.png'))
+  })
+}
