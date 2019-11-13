b7da3a945b73e833140cdaf1a04e3ee14e7c531a
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-11-13 17:13:08 +0100

    encrypt or decrypt based on cmd inputs

--- A chaotic permutation and diffusion based image encryption algorithm for secure communications/index.js
+++ A chaotic permutation and diffusion based image encryption algorithm for secure communications/index.js
@@ -1,14 +1,32 @@
-const getPixels = require('get-pixels')
+const fs = require('fs')
+const imread = require('get-pixels')
 // const imshow = require('ndarray-imshow')
 const imshow = require('gnuplot-imshow')
 const imhist = require('ndarray-imhist')
+const imwrite = require('save-pixels')
 
 const encrypt = require('./encrypt')
 
-getPixels('../images/lena_gray_128.png', (err, pixels) => {
-  imshow(pixels)
-  imhist(pixels)
-  pixels = encrypt(pixels)
-  imhist(pixels)
-  imshow(pixels, {gray: true})
-})
+if (process.argv.includes('decryption')) {
+  decryption()
+} else {
+  encryption()
+}
+
+function encryption () {
+  imread('../images/lena_gray_128.png', (err, pixels) => {
+    imshow(pixels)
+    imhist(pixels)
+  
+    pixels = encrypt(pixels)
+    imhist(pixels)
+  
+    imshow(pixels, {gray: true})
+    imwrite(pixels, 'png')
+      .pipe(fs.createWriteStream('cipher-image.png'))
+  })
+}
+
+function decryption () {
+  console.log('Not yet implemented')
+}
