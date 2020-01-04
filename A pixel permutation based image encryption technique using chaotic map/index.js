0ee1c9345c78d6e70bdfc392d7d914ec1d4fbe01
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2020-01-04 11:03:40 +0100

    export the cipher image after encryption

--- A pixel permutation based image encryption technique using chaotic map/index.js
+++ A pixel permutation based image encryption technique using chaotic map/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const imread = require('get-pixels')
-// const imshow = require('ndarray-imshow')
 const imshow = require('gnuplot-imshow')
 const imhist = require('ndarray-imhist')
 const imwrite = require('save-pixels')
@@ -17,10 +16,13 @@ function encryption () {
   imread('../images/lena_gray_256.png', async (err, pixels) => {
     imshow(pixels)
     imhist(pixels)
-  
+
     pixels = await encrypt(pixels, {times: 1})
     imhist(pixels)
     imshow(pixels)
+
+    imwrite(pixels, 'png')
+      .pipe(fs.createWriteStream('cipher-image.png'))
   })
 }
 
